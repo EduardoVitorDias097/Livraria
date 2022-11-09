4e5bf7dacc8abeec5c794b1fb01adcc16a6a3459
EduardoVitorDias097 <duduvitordias@gmail.com>
2022-11-09 08:36:34 -0300

    Habilitando Swagger e Redoc usando DRF Spectacular

--- livraria/urls.py
+++ livraria/urls.py
@@ -15,6 +15,12 @@ from django.conf.urls.static import static
 
 from uploader.router import router as uploader_router
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
 router.register(r'editoras', EditoraViewSet)
@@ -26,8 +32,22 @@ urlpatterns = [
     path('', include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/media/", include(uploader_router.urls))
+    path("api/media/", include(uploader_router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     
+    path("api/", include(router.urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
+
