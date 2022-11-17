0df6c874d0242cafbdeacb8c99f5e316149f4a9a
Eduardo <duduvitordias@gmail.com>
2022-11-17 13:24:07 -0300

    Arrumando swagger

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -6,11 +6,7 @@ from uploader.serializers import ImageSerializer
 
 
 class LivroSerializer(ModelSerializer):
-    class Meta:
-        model = Livro
-        fields = "__all__"
 
-class LivroSerializer(ModelSerializer):
     capa_attachment_key = SlugRelatedField(
         source="capa",
         queryset=Image.objects.all(),
@@ -19,6 +15,11 @@ class LivroSerializer(ModelSerializer):
         write_only=True,
     )
     capa = ImageSerializer(required=False, read_only=True)
+    
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
 
 class LivroDetailSerializer(ModelSerializer):
         capa = ImageSerializer(required=False)
